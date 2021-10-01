2a47a81444cedd693a572a565358de8cffa38e88
Anmol Manocha <60709260+AnmolManocha@users.noreply.github.com>
2021-10-01 12:45:52 +0530

    Update Linear Search.c

--- DSA/C/Linear Search.c
+++ DSA/C/Linear Search.c
@@ -5,22 +5,23 @@
 #include <stdio.h>
 int main()
 {
-    int arr[10], i, n, element;
+    int n, element;
     printf("ENTER THE NO. OF ELEMENTS:");
     scanf("%d", &n);
+    int arr[n];
     printf("ENTER ARRAY ELEMENTS:\n");
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
     }
     printf("ENTERED ARRAY ELEMENTS ARE: \n");
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         printf("%d \n", arr[i]);
     }
     printf("ENTER THE ELEMENT TO BE SEARCHED:");
     scanf("%d", &element);
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         if (arr[i] == element)
         {
