6ad215ddb9840ea01027ba055ba945e3e2f7cdf4
Raghav Rastogi <51928619+raghav2404@users.noreply.github.com>
2021-10-01 15:04:56 +0530

    Create binarySearch.java

---
+++ DSA/Java/binarySearch.java
@@ -0,0 +1,34 @@
+/*
+    Author: Raghav Rastogi
+    Date: 01/10/2021
+ */
+
+class BinarySearch {
+    int binarySearch(int arr[], int l,
+                          int r, int x)
+    {
+        if (r >= l && l<arr.length-1) {
+           
+            int mid = (l+r)>>1;
+            if (arr[mid] == x)
+                return mid;
+           else if (arr[mid] > x)
+                return binarySearch(arr, l, mid - 1, x);
+            else
+              return binarySearch(arr, mid + 1, r, x);
+        }
+        return -1;
+    }
+   public static void main(String args[])
+    {
+        BinarySearch ob = new BinarySearch();
+        int arr[] = { 2, 3, 4, 10, 40 };
+        int n = arr.length;
+        int x = 10;
+        int result = ob.binarySearch(arr, 0, n - 1, x);
+        if (result == -1)
+            System.out.println("Element not present");
+        else
+            System.out.println("Element found at index " + result);
+    }
+}
