3883e5571de117edf8308c867af8bf89ac681c6b
Mayank Ujawane <67855648+MayankUjawane@users.noreply.github.com>
2021-10-02 09:30:05 +0530

    create Preorder Traversal

---
+++ DSA/Java/Trees/Preorder Traversal.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Trees {
+    TreeNode root;
+
+    public static void main(String[] args) {
+        //Creating Tree
+        Trees tree = new Trees();
+        tree.root = new TreeNode(1);
+        tree.root.left = new TreeNode(2);
+        tree.root.right = new TreeNode(3);
+        tree.root.left.left = new TreeNode(4);
+        tree.root.left.right = new TreeNode(5);
+        //Calling printPreorder method to print Preorder Traversal of Tree
+        tree.printPreorder(tree.root);
+    }
+
+    private void printPreorder(TreeNode node) {
+        if (node == null) return;
+
+        System.out.println(node.val);
+        printPreorder(node.left);
+        printPreorder(node.right);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
