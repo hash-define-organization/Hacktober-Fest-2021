b27ef60965f04ff8e45fc694e8ec42c59ed82821
kartik <kartik.malik@imarticus.com>
2021-10-03 09:28:04 +0530

    added

--- javascript/promise-sample.js
+++ javascript/promise-sample.js
@@ -2,7 +2,7 @@ const p = new Promise((resolve, reject) => {
   resolve("hi");
 });
 p.then((data) => {
-  console.log("hi");
+  console.log(data);
 }).catch((err) => {
   console.log(err.message);
 });
