6e1fb3f24cfe9e1c1226befe826f60d727b820d1
Sidarth-Roy <73904891+Sidarth-Roy@users.noreply.github.com>
2021-10-10 23:59:04 +0530

    Add files via upload

---
+++ DSA/C/Project/TicTacToe.c
@@ -0,0 +1,103 @@
+//WORKS ON CODEBLOCKS NOT IN ONLINE COMPILERS
+//Add sound
+
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+//#include<windows.h>
+//#include<mmsystem.h>
+
+
+char square[10] = {'o','1','2','3','4','5','6','7','8','9'};
+int checkWin();
+void drawBoard();
+
+int main(){
+	system("color 0D");
+	int player = 1, i ,choice;
+	char mark; // X,O
+	do {
+		drawBoard();
+		player = (player % 2) ? 1 : 2;
+		printf("Player %d, enter the choice : ",player);
+		scanf("%d",&choice);
+		mark = (player == 1) ? 'X' : 'O';
+		if(choice == 1 && square[1] == '1')
+			square[1] = mark;
+		    else if(choice == 2 && square[2] == '2')
+			square[2] = mark;
+			else if(choice == 3 && square[3] == '3')
+			square[3] = mark;
+			else if(choice == 4 && square[4] == '4')
+			square[4] = mark;
+			else if(choice == 5 && square[5] == '5')
+			square[5] = mark;
+			else if(choice == 6 && square[6] == '6')
+			square[6] = mark;
+			else if(choice == 7 && square[7] == '7')
+			square[7] = mark;
+			else if(choice == 8 && square[8] == '8')
+			square[8] = mark;
+			else if(choice == 9 && square[9] == '9')
+			square[9] = mark;
+
+			else {
+				printf("Invalid option !");
+				player--;                       // to again provide chance to the player to enter the choice
+		//		getch();
+			}
+			i = checkWin();
+			player++;
+
+	}while(i == -1);
+
+	drawBoard();
+	if(i==1){
+		printf("==>Player %d won",--player);
+		//PlaySound(TEXT("C:\\Users\\roy\\Music\\Winner.wave"),NULL,SND_SYNC);          //ADD -lwinmm in linker
+
+	}
+	else {
+		printf("==>Game draw");
+	}
+//getch();
+return 0;
+}
+int checkWin(){
+	if(square[1] == square[2] && square[2] == square[3])
+		return 1;
+	else if (square[4] == square[5] && square[5] == square[6])
+		return 1;
+	else if(square[7] == square[8] && square[8] == square[9])
+		return 1;
+	else if(square[1] == square[4] && square[4] == square[7])
+		return 1;
+	else if(square[2] == square[5] && square[5] == square[8])
+		return 1;
+	else if(square[3] == square[6] && square[6] == square[9])
+		return 1;
+	else if(square[1] == square[5] && square[5] == square[9])
+		return 1;
+	else if(square[3] == square[5] && square[5] == square[7])
+		return 1;
+	else if(square[1] != '1' && square[2] != '2' && square[3] != '3' && square[4] !='4' && square[5] != '5' && square[6] != '6' && square[7] != '7' && square[8] != '8' && square[9] != '9')
+		return 0;          // case of draw
+	else
+		return -1;         //game is still on
+}
+
+void drawBoard(){
+	//system("cls");
+	// clrscr();
+	printf("\n\n\t Tic Tac Toe \n\n");
+	printf("Player1 (X) - Player2 (O) \n\n\n");
+	printf("     |     |     \n");
+	printf("  %c  |  %c  |  %c  \n",square[1],square[2],square[3]);
+	printf("_____|_____|_____\n");
+	printf("     |     |     \n");
+	printf("  %c  |  %c  |  %c  \n",square[4],square[5],square[6]);
+	printf("_____|_____|_____\n");
+	printf("     |     |     \n");
+	printf("  %c  |  %c  |  %c  \n",square[7],square[8],square[9]);
+	printf("     |     |     \n");
+}
